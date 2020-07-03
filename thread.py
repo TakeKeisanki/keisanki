9b37491ccc5ebdc9132a70fa82e5ea6581185680
TakeKeisanki <67773581+TakeKeisanki@users.noreply.github.com>
2020-07-03 13:56:51 +0900

    thread safe
    using the Rlock()

--- thread.py
+++ thread.py
@@ -2,15 +2,17 @@ from threading import Thread
 
 def my_function1():
   for i in range(10000):
-    print("123")
-    print("456")
-    print("789")
+    with lk1:
+      print("123")
+      print("456")
+      print("789")
 
 def my_function2():
   for i in range(10000):
-    print("abc")
-    print("def")
-    print("ghi")
+    with lk2:
+      print("abc")
+      print("def")
+      print("ghi")
 
 def my_function3():
   for i in range(10000):
@@ -19,6 +21,8 @@ def my_function3():
     print("GHI")
 
 if __name__ == "__main__":
+  lk1 = Rlock()
+  lk2 = Rlock()
   t1 = Thread(target = my_function1)
   t2 = Thread(target = my_function2)
   t3 = Thread(target = my_function3)
